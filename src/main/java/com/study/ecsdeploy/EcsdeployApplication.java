96bb70861434f83d84ee81dbcd32f00505b5b1b0
woo jin <dnwls16071@naver.com>
2026-07-18 03:14:33 +0900

    chore: scaffold Spring Boot project with Gradle wrapper

---
+++ src/main/java/com/study/ecsdeploy/EcsdeployApplication.java
@@ -0,0 +1,13 @@
+package com.study.ecsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/ecsdeploy/EcsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ecsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
